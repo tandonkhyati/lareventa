4d0a1d415109208eeed0700c396c69a9badd6e0e
khyati tandon <khyati tandon>
2017-03-25 15:03:32 +0530

    update

---
+++ LaReventia/LaReventia/app/src/main/java/com/projectfinal/profile/lareventia/MainActivity.java
@@ -0,0 +1,26 @@
+package com.projectfinal.profile.lareventia;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final boolean b=new Handler().postDelayed(new Runnable(){
+
+
+            @Override
+            public void run() {
+                Intent i=new Intent(MainActivity.this,Login.class);
+                startActivity(i);
+                finish();
+            }
+        },3000);
+
+    }
+}
